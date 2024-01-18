3549127e0702035b772edd66494112c1090d4e6c
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-01-18 15:15:47 -0500

    test(backend): test cases in comment

---
+++ backend/app/Http/Controllers/CommentControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommentControllerTest extends TestCase
+{
+     use DatabaseTransactions;
+
+     public function test_can_delete_comment(): void
+     {
+          $user = User::factory()->create();
+          $post = Post::factory()->create();
+          $comment = Comment::factory()->create([
+               'post_id' => $post->id,
+               'user_id' => $user->id,
+          ]);
+
+          $response = $this->actingAs($user)
+               ->deleteJson('/api/posts/' . $post->id . '/comments/' . $comment->id);
+
+          $response->assertStatus(200);
+          $response->assertJson([
+               'message' => 'Comment deleted successfully',
+          ]);
+          $this->assertDatabaseMissing('comments', [
+               'id' => $comment->id,
+          ]);
+     }
+
+     public function test_cannot_delete_comment_if_not_authorized(): void
+     {
+          $user1 = User::factory()->create();
+          $user2 = User::factory()->create();
+          $post = Post::factory()->create();
+          $comment = Comment::factory()->create([
+               'post_id' => $post->id,
+               'user_id' => $user1->id,
+          ]);
+
+          $response = $this->actingAs($user2)
+               ->deleteJson('/api/posts/' . $post->id . '/comments/' . $comment->id);
+
+          $response->assertStatus(403);
+          $response->assertJson([
+               'message' => 'You do not own this comment.',
+          ]);
+          $this->assertDatabaseHas('comments', [
+               'id' => $comment->id,
+          ]);
+     }
+}
