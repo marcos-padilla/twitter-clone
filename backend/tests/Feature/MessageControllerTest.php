4dec4478689e1fd42188d9ea0a4c9533defc012e
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-01-21 20:56:04 -0500

    test(backend): test cases for message

---
+++ backend/tests/Feature/MessageControllerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MessageControllerTest extends TestCase
+{
+
+    use DatabaseTransactions;
+
+    public function test_user_can_send_message(): void
+    {
+        $sender = User::factory()->create();
+        $receiver = User::factory()->create();
+
+        $response = $this->actingAs($sender)->postJson("/api/message/$receiver->id", [
+            'message' => 'Hello World',
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('messages', [
+            'sender_id' => $sender->id,
+            'receiver_id' => $receiver->id,
+            'message' => 'Hello World',
+        ]);
+    }
+
+    public function test_user_can_see_all_messages_with_another_user(): void
+    {
+        $sender = User::factory()->create();
+        $receiver = User::factory()->create();
+
+        $this->actingAs($sender)->postJson("/api/message/$receiver->id", [
+            'message' => 'Hello Reciever',
+        ]);
+
+        $this->actingAs($receiver)->postJson("/api/message/$sender->id", [
+            'message' => 'Hello Sender',
+        ]);
+
+        $response = $this->actingAs($sender)->getJson("/api/message/$receiver->id");
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'sender_id',
+                    'receiver_id',
+                    'message',
+                    'created_at',
+                    'updated_at',
+                ]
+            ]
+        ]);
+
+        $response->assertJson([
+            'data' => [
+                [
+                    'sender_id' => $sender->id,
+                    'receiver_id' => $receiver->id,
+                    'message' => 'Hello Reciever',
+                ],
+                [
+                    'sender_id' => $receiver->id,
+                    'receiver_id' => $sender->id,
+                    'message' => 'Hello Sender',
+                ]
+            ]
+        ]);
+    }
+}
